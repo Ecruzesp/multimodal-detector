2f301d61ce8f39994452622ee1c2b4eb452c9467
Ecruzesp <87870423+Ecruzesp@users.noreply.github.com>
2026-04-27 11:57:25 -0600

    Add function to convert depth map to point cloud

--- utils/geometry.py
+++ utils/geometry.py
@@ -1,1 +1,11 @@
+import torch
 
+def depth_to_pointcloud(depth, K):
+    H, W = depth.shape
+    y, x = torch.meshgrid(torch.arange(H), torch.arange(W), indexing='ij')
+
+    z = depth
+    x3d = (x - K[0,2]) * z / K[0,0]
+    y3d = (y - K[1,2]) * z / K[1,1]
+
+    return torch.stack([x3d, y3d, z], dim=-1).reshape(-1, 3)
